c6038a3f16c7d54a7f4aa1407ab714a03a723a92
chrisryda <chrisryda@hotmail.com>
2022-09-30 11:52:54 +0200

    Completed ex4 task3

---
+++ ex4/3/more_functions.c
@@ -0,0 +1,11 @@
+#include "more_functions.h"
+#include <stdio.h>
+
+void another_function() {
+  printf("You have called another_function\n");
+}
+
+void yet_another_function() {
+  printf("You have called yet_another_function\n");
+}
+
