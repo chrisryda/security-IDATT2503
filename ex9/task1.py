4f36533dc91369930513ea929c9c61c26a292c5a
chrisryda <chrisryda@hotmail.com>
2022-11-10 22:19:32 +0100

    Fixed hmac, thanks norbi

--- ex9/task1.py
+++ ex9/task1.py
@@ -3,17 +3,17 @@ IPAD = 0b0011
 OPAD = 0b0101
 
 def h(x):
-    return ((x**2)%(2**8))
+    return ((x*x % 2**8) & 0b00111100) >> 2
 
-def hmac(K, x): 
-    res = ((h(K ^ OPAD)<<8 | h(K ^ IPAD))<<8 | x)
-    return "00" + bin(h(res))[2:]
+def hmac(K, x):
+    res = h((K ^ OPAD)<<8 | h((K ^ IPAD)<<8 | x))
+    return bin(res)
 
 msg = 0b0110
 res = hmac(K, msg)
-print(f"Task a:\nFull HMAC: {res}\nCenter 4 digits: {res[2:6]}")
+print(f"Task a:\nHMAC: {res}")
 
 msg_recieved = 0b0111
 res2 = hmac(K, msg_recieved)
-print(f"\nTask b:\nRecieved HMAC: 0100\nCalculated HMAC: {res2[2:6]}")
+print(f"\nTask b:\nRecieved HMAC: {bin(msg_recieved)}\nCalculated HMAC: {res2}")
 print("\nAt HMAC er ulike tyder på at avsenderen av meldingen ikke har samme hashfunksjon som oss.\nDette tyder på at meldingen ikke er autentisk.")
